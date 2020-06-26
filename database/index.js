2d9a0d91e611141d4ddcc35d3d800e8605c29d96
Deepankar <deepankar080892@gmail.com>
2020-06-26 18:38:31 +0200

    setup for production

--- database/index.js
+++ database/index.js
@@ -1,9 +1,20 @@
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        'host': 'localhost',
-        database: 'todo_db'
-    }
-});
+const options =
+  process.env.NODE_ENV === 'production'
+    ? {
+        client: 'pg',
+        connection: {
+          host: 'localhost',
+          database: 'todo_db',
+        },
+      }
+    : {
+        client: 'pg',
+        connection: {
+          host: 'localhost',
+          database: 'todo_db',
+        },
+      };
+
+const knex = require('knex')(options);
 
 module.exports = knex;
--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use('/api', apiRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
-    res.send('Hi There!');
-});
-
+  const path = require('path');
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
