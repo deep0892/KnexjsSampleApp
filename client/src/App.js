033e60a8d00c1b9fbb4a2214dfb2085cca5b0859
Deepankar <deepankar080892@gmail.com>
2020-06-26 11:13:19 +0200

    Setting react app

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="App">This is a sample React App</div>;
+}
+
+export default App;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const apiRoute = require('./routes/api');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
